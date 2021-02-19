6b899cebce249df58a4ffb205f6e0ea33179aba7
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-19 01:50:42 -0800

    Get ping to work for https://github.com/fullstack-hy2020/patientor, also send data so page can render

--- index.ts
+++ index.ts
@@ -2,11 +2,26 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
+});
+
+app.get('/api/patients', (_req, res) => {
+    console.log('someone pinged here');
+    console.log('patients pong');
+    res.send(JSON.stringify(
+        [{
+            "id": "d2773336-f723-11e9-8f0b-362b9e155667",
+            "name": "John McClane",
+            "dateOfBirth": "1986-07-09",
+            "ssn": "090786-122X",
+            "gender": "male",
+            "occupation": "New york city cop"
+        }]
+    ));
 });
 
 app.listen(PORT, () => {
