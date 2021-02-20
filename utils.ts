44a03e206a6b979ed923aaf6a4a9c476b652128a
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-20 02:59:10 -0800

    9.13: Patientor backend, step6 Set up safe parsing, validation and type guards to the POST /api/patients request.  Refactor the Gender field to use an enum type.

--- utils.ts
+++ utils.ts
@@ -1,32 +1,61 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-import { Patient, NewPatient } from './types';
+import { Patient, NewPatient, Gender } from './types';
 
 const parseInput = (input: any, property: string): string => {
     if (!input || !isString(input)) {
-        // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
         throw new Error("Incorrect or missing input: " + input + ' for property ' + property);
     }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return input;
+};
+
+const isGender = (param: any): Boolean => {
+    return Object.values(Gender).includes(param);
+}
+
+const parseGender = (gender: any): Gender => {
+    if (!gender || !isGender(gender)) {
+        throw new Error('Incorrect or missing gender: ' + gender);
+    }
+    return gender;
 };
 
 const isString = (text: any): boolean => {
     return typeof text === 'string' || text instanceof String;
 };
 
-const toNewPatient = (object: NewPatient): Patient => {
+const isDate = (date: string): boolean => {
+    return Boolean(Date.parse(date));
+};
 
-    for (let x in object) {
-        console.log(x);
+const parseDate = (date: any): string => {
+    if (!date || !isString(date) || !isDate(date)) {
+        throw new Error('Incorrect or missing date: ' + date);
     }
+    return date;
+};
 
+const isNumeric = (num: any): boolean => {
+    return !isNaN(num);
+};
+
+const parseSsn = (ssn: any): string => {
+    if (!isNumeric(ssn)) throw new Error('Incorrect not numeric: ' + ssn);
+    if (!isString(ssn)) return ssn + "";
+
+    return ssn;
+};
+
+const toNewPatient = (object: NewPatient): Patient => {
     const newPatient: Patient = {
         id: `${Date.now()}`,
-        ...object,
+        gender: parseGender(object.gender),
         name: parseInput(object.name, "name"),
         occupation: parseInput(object.occupation, "occupation"),
-
+        dateOfBirth: parseDate(object.dateOfBirth),
+        ssn: parseSsn(object.ssn)
     };
 
     return newPatient;
